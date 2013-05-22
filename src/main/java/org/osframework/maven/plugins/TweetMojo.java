921c7bd82abf0a3f598d477c1fe6f15aa426d6cc
Dave Joyce <dave@osframework.org>
2013-05-22 02:13:27 -0400

    First pass at completed, functional TweetMojo

--- src/main/java/org/osframework/maven/plugins/TweetMojo.java
+++ src/main/java/org/osframework/maven/plugins/TweetMojo.java
@@ -17,10 +17,25 @@
  */
 package org.osframework.maven.plugins;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.project.MavenProject;
 import org.codehaus.plexus.util.StringUtils;
+
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.auth.AccessToken;
+import twitter4j.auth.RequestToken;
 
 /**
  * Sends a message to Twitter (aka 'tweet') via the Twitter API v1.1.
@@ -31,51 +46,123 @@ import org.codehaus.plexus.util.StringUtils;
  * @requiresOnline true
  * 
  * @author <a href="mailto:dave@osframework.org">Dave Joyce</a>
+ * @see <a href="https://dev.twitter.com/docs/api/1.1">Twitter - REST API v1.1 Resources</a>
  */
 public class TweetMojo extends AbstractMojo {
 
 	/**
-	 * Twitter account username.
-	 * @parameter expression="${twitter.accountName}"
+	 * Reference to the enclosing Maven project.
+	 * 
+	 * @parameter default-value="${project}"
 	 * @required
+	 * @readonly
 	 */
-	private String accountName;
+	private MavenProject project;
 
 	/**
-	 * Application ID registered on Twitter for this project.
-	 * @parameter expression="${twitter.applicationId}"
+	 * Generated consumer key for this project.
+	 * 
+	 * @parameter expression="${consumerKey}"
 	 * @required
 	 */
-	private String applicationId;
+	private String consumerKey;
 
 	/**
-	 * Generated client token for this project.
-	 * @parameter expression="${twitter.clientToken}"
+	 * Generated consumer secret for this project.
+	 * 
+	 * @parameter expression="${consumerSecret}"
 	 * @required
 	 */
-	private String clientToken;
+	private String consumerSecret;
 
 	/**
-	 * Generated client secret for this project.
-	 * @parameter expression="${twitter.clientSecret}"
-	 * @required
+	 * Generated access token for this project.
+	 * 
+	 * @parameter expression="${accessToken}"
 	 */
-	private String clientSecret;
+	private String accessToken;
+
+	/**
+	 * Generated access token secret for this project.
+	 * 
+	 * @parameter expression="${accessTokenSecret}"
+	 */
+	private String accessTokenSecret;
+
+	/**
+	 * Text of status ('tweet') to post to Twitter.
+	 * 
+	 * @parameter expression="${message}" default-value="${project.artifactId}:${project.version} released"
+	 */
+	private String message;
 
 	public void execute() throws MojoExecutionException, MojoFailureException {
 		// Validate required parameters
-		if (StringUtils.isEmpty(accountName)) {
-			throw new MojoFailureException("Missing required parameter 'accountName'");
-		} else if (StringUtils.isEmpty(applicationId)) {
-			throw new MojoFailureException("Missing required parameter 'applicationId'");
-		} else if (StringUtils.isEmpty(clientToken)) {
-			throw new MojoFailureException("Missing required parameter 'clientToken'");
-		} else if (StringUtils.isEmpty(clientSecret)) {
-			throw new MojoFailureException("Missing required parameter 'clientSecret'");
+		if (null == project) {
+			throw new MojoExecutionException("Expected POM reference is null");
 		}
-		
-		getLog().info("Tweeting as '" + accountName + "'");
-		getLog().info("A new tweet would be posted now...");
+		if (StringUtils.isBlank(consumerKey)) {
+			throw new MojoFailureException("Missing required parameter 'consumerKey'");
+		} else if (StringUtils.isBlank(consumerSecret)) {
+			throw new MojoFailureException("Missing required parameter 'consumerSecret'");
+		}
+		Twitter twitter = TwitterFactory.getSingleton();
+		twitter.setOAuthConsumer(consumerKey, consumerSecret);
+		try {
+			// Set access token
+			AccessToken oauthToken = acquireAccessToken(twitter);
+			twitter.setOAuthAccessToken(oauthToken);
+			getLog().info("Authenticated; posting tweet...");
+			
+			Status status = twitter.updateStatus(createStatus());
+			getLog().info("Posted tweet. Details:");
+			getLog().info("* Created at: " + formatDate(status.getCreatedAt()));
+			getLog().info("* Text:       '" + status.getText() + "'");
+		} catch (IOException ioe) {
+			getLog().error("Failed to read PIN from input");
+			throw new MojoFailureException("Failed to read PIN from input", ioe);
+		} catch (TwitterException te) {
+			getLog().error("Failed to post tweet. Details:");
+			getLog().error("Message: " + te.getErrorMessage());
+			getLog().error("Code: " + te.getExceptionCode());
+			throw new MojoExecutionException("Failed to post tweet.", te);
+		}
+	}
+
+	private AccessToken acquireAccessToken(Twitter twitter) throws IOException, TwitterException {
+		AccessToken token = null;
+		if (StringUtils.isNotBlank(accessToken) && StringUtils.isNotBlank(accessTokenSecret)) {
+			token = new AccessToken(accessToken, accessTokenSecret);
+		} else {
+			RequestToken requestToken = twitter.getOAuthRequestToken();
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			while (null == token) {
+				getLog().info("Open the following URL and grant access to your account:");
+				getLog().info(requestToken.getAuthorizationURL());
+				System.out.print("Enter the PIN (if available) or just hit enter. [PIN]:");
+				String pin = br.readLine();
+				token = (0 < pin.length())
+						 ? twitter.getOAuthAccessToken(requestToken, pin)
+						 : twitter.getOAuthAccessToken();
+			}
+		}
+		return token;
+	}
+
+	private String createStatus() {
+		String status = (StringUtils.isNotBlank(message))
+				         ? message
+				         : new StringBuilder(project.getArtifactId())
+		                       .append(":")
+		                       .append(project.getVersion())
+		                       .append(" released")
+		                       .toString();
+		return status;
+	}
+
+	private String formatDate(Date d) {
+		final DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");
+		return df.format(d);
 	}
 
 }
